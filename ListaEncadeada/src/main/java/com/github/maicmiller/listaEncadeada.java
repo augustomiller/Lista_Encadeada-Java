cab91c36ecaffd3e391a29b3ee78392015994a7e
Maic Miller <maicmiller42@gmail.com>
2021-08-02 16:20:11 -0300

    Implementação da Classe ListaEncadeada.
    - implementação do método adicionar.

--- ListaEncadeada/src/main/java/com/github/maicmiller/listaEncadeada.java
+++ ListaEncadeada/src/main/java/com/github/maicmiller/listaEncadeada.java
@@ -8,6 +8,23 @@ public class listaEncadeada<T> {
         this.referenciaEntrada = null;
     }
 
+    public void add(T conteudo){
+        No<T> novoNo = new No<>(conteudo);
+        if (this.isEmpty()){ // Se a lista estiver vazia...
+            referenciaEntrada = novoNo; // Eu digo que a referencia de entrada é igual ao novo nó...
+            return;
+        }
+        // Se ñao, continua...
+        No<T> noAuxiliar = referenciaEntrada;
+
+        for (int i = 0; i < this.size() -1 ; i++){
+            noAuxiliar = noAuxiliar.getProximoNo();
+        }
+        
+        noAuxiliar.setProximoNo(novoNo);
+
+    }
+
     public int size(){ // Método responsável por retornar o tamanho da lista...
         int tamanhoLista = 0;
         No<T> referenciaAux = referenciaEntrada;
