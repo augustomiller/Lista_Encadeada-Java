58d9a30e2e5acc36382e2d042c26b7fc5ac479d5
Maic Miller <maicmiller42@gmail.com>
2021-08-02 17:55:27 -0300

    :sparkles: Implementando a classe Main.
    Implementando a classe main para utilizar os métodos criados...

--- ListaEncadeada/src/main/java/com/github/maicmiller/Main.java
+++ ListaEncadeada/src/main/java/com/github/maicmiller/Main.java
@@ -12,6 +12,19 @@ public class Main {
         minhaListaEncadeada.add("teste4");
 
         System.out.println(minhaListaEncadeada.get(0));
+        System.out.println(minhaListaEncadeada.get(1));
+        System.out.println(minhaListaEncadeada.get(2));
+        System.out.println(minhaListaEncadeada.get(3));
+
+        // Imprimindo a Lista completa...
+        System.out.println(minhaListaEncadeada);
+
+        // Removendo o Nó de índice número 3...
+        System.out.println(minhaListaEncadeada.remove(3));
+        System.out.println(minhaListaEncadeada);
+
+
+
 
     }
 }
